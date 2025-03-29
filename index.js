036d97935873730e9fb3d577116710d4038baae5
VenusFlyTrap <79674167+VFT369@users.noreply.github.com>
2025-03-30 00:50:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControles.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
-
 const fov = 75;
 const aspect = w / h;
 const near = 0.1;
@@ -14,9 +14,32 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.33;
+
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+});
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
-renderer.render(scene, camera);
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0x0f0f0f0f,
+    wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001)
+mesh.add(wireMesh)
+
+const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+scene.add(hemiLight);
+
+function animate(t = 0) {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    Controls.update();
+}
+animate();
